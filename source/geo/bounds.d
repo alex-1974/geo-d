c93451e090de0aeb75ede64c8df6afd19e9968b3
Alexander Bernardi <alexander.leisser.bernardi@gmail.com>
2026-09-11 12:36:22 +0200

    docs: document Segment2 and Bounds2 semantics

--- source/geo/bounds.d
+++ source/geo/bounds.d
@@ -37,6 +37,10 @@ if (isGeoScalar!T)
 /**
  * A closed axis-aligned bounds in a two-dimensional Euclidean space.
  *
+ * Supported scalar types are `int`, `long`, `float`, `double`, and `real`.
+ *
+ * `Bounds2.init` is empty.
+ *
  * Bounds2 has two semantic states:
  *
  * - empty: contains no point;
